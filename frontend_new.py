90caeaeaef640dc799bb76793ff98340d9037f4b
Ganesh Shankar Kende <143523277+Ganu7249@users.noreply.github.com>
2025-05-08 00:41:29 +0530

    Update frontend_new.py

--- frontend_new.py
+++ frontend_new.py
@@ -1,6 +1,5 @@
 import requests
 
 files = {"file": open("image.png", "rb")}
-res = requests.post("https://<ganeshkende>.huggingface.co/spaces/<pomegranate-disease-detection-app-backend>/predict
-", files=files)
+res = requests.post("https://<ganeshkende>.huggingface.co/spaces/<pomegranate-disease-detection-app-backend>/predict", files=files)
 prediction = res.json()
